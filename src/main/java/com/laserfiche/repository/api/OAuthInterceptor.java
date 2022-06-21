d75bcf6b0e897a54a8332dfe147e346696530099
Jason Jia <jason.jia@laserfiche.com>
2022-06-21 16:10:52 -0400

    Add an interceptor to handle authorization

--- src/main/java/com/laserfiche/repository/api/OAuthInterceptor.java
+++ src/main/java/com/laserfiche/repository/api/OAuthInterceptor.java
@@ -1,13 +1,37 @@
 package com.laserfiche.repository.api;
 
+import com.laserfiche.api.client.httphandlers.BeforeSendResult;
+import com.laserfiche.api.client.httphandlers.OAuthClientCredentialsHandler;
+import com.laserfiche.api.client.httphandlers.Request;
+import com.laserfiche.api.client.httphandlers.RequestImpl;
+import com.laserfiche.api.client.model.AccessKey;
 import okhttp3.Interceptor;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class OAuthInterceptor implements Interceptor {
+    private final OAuthClientCredentialsHandler oauthHandler;
+
+    public OAuthInterceptor(String servicePrincipalKey, AccessKey accessKey) {
+        oauthHandler = new OAuthClientCredentialsHandler(servicePrincipalKey, accessKey);
+    }
+
     @Override
     public Response intercept(Chain chain) throws IOException {
-        return null;
+        okhttp3.Request originalRequest = chain.request();
+        Request customRequest = new RequestImpl();
+        CompletableFuture<BeforeSendResult> future = oauthHandler.beforeSendAsync(customRequest);
+        try {
+            future.get(); // We are blocked by the HTTP handler
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        okhttp3.Request newRequest = originalRequest.newBuilder()
+                .addHeader("Authorization", customRequest.headers().get("Authorization"))
+                .build();
+        return chain.proceed(newRequest);
     }
 }
