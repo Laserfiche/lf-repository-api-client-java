33def91e07474f12f8c93177ff63aee1bbae9390
Jason Jia <jason.jia@laserfiche.com>
2022-06-22 10:18:52 -0400

    Add interface for the client

--- src/main/java/com/laserfiche/repository/api/RepositoryApiClient.java
+++ src/main/java/com/laserfiche/repository/api/RepositoryApiClient.java
@@ -1,5 +1,63 @@
 package com.laserfiche.repository.api;
 
-public class RepositoryApiClient extends ApiClient {
+import com.laserfiche.repository.api.client.*;
 
+/**
+ * The Laserfiche Repository API client.
+ */
+public interface RepositoryApiClient {
+    /**
+     * @return The Laserfiche Repository Attributes API client.
+     */
+    AttributesApi getAttributesClient();
+
+    /**
+     * @return The Laserfiche Repository Audit Reasons API client.
+     */
+    AuditReasonsApi getAuditReasonsClient();
+
+    /**
+     * @return The Laserfiche Repository Entries API client.
+     */
+    EntriesApi getEntriesClient();
+
+    /**
+     * @return The Laserfiche Repository Field Definitions API client.
+     */
+    FieldDefinitionsApi getFieldDefinitionsClient();
+
+    /**
+     * @return The Laserfiche Repository Repositories API client.
+     */
+    RepositoriesApi getRepositoryClient();
+
+    /**
+     * @return The Laserfiche Repository Searches API client.
+     */
+    SearchesApi getSearchesClient();
+
+    /**
+     * @return The Laserfiche Repository Server Session API client.
+     */
+    ServerSessionApi getServerSessionClient();
+
+    /**
+     * @return The Laserfiche Repository Simple Searches API client.
+     */
+    SimpleSearchesApi getSimpleSearchesClient();
+
+    /**
+     * @return The Laserfiche Repository Tag Definitions API client.
+     */
+    TagDefinitionsApi getTagDefinitionsClient();
+
+    /**
+     * @return The Laserfiche Repository Tasks API client.
+     */
+    TasksApi getTasksClient();
+
+    /**
+     * @return The Laserfiche Repository Template Definitions API client.
+     */
+    TemplateDefinitionsApi getTemplateDefinitionClient();
 }
