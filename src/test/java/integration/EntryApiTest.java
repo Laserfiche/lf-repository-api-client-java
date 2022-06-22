325631da18d0d28a560f59cfd868e9ef413d8997
Jason Jia <jason.jia@laserfiche.com>
2022-06-22 17:08:25 -0400

    Add a minimal test for entry APIs

---
+++ src/test/java/integration/EntryApiTest.java
@@ -0,0 +1,23 @@
+package integration;
+
+import com.laserfiche.repository.api.client.EntriesApi;
+import com.laserfiche.repository.api.client.model.Entry;
+import org.junit.jupiter.api.Test;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class EntryApiTest extends BaseTest {
+
+    @Test
+    public void getEntry_Success() throws IOException {
+        EntriesApi entriesClient = repositoryApiClient.getEntriesClient();
+        Call<Entry> call = entriesClient.getEntry(repoId, 1, null);
+        Response<Entry> entry = call.execute();
+
+        assertNotNull(entry);
+    }
+}
