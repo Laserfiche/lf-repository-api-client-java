1d03574d879c8797a6b68958c38775681a16f232
Jason Jia <jason.jia@laserfiche.com>
2022-06-22 13:47:55 -0400

    Add implementation for the client

---
+++ src/main/java/com/laserfiche/repository/api/RepositoryApiClientImpl.java
@@ -0,0 +1,149 @@
+package com.laserfiche.repository.api;
+
+import com.laserfiche.api.client.httphandlers.HttpRequestHandler;
+import com.laserfiche.api.client.httphandlers.OAuthClientCredentialsHandler;
+import com.laserfiche.api.client.model.AccessKey;
+import com.laserfiche.repository.api.client.*;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class RepositoryApiClientImpl implements RepositoryApiClient {
+    private final HttpRequestHandler oauthHandler;
+    private final Retrofit.Builder clientBuilder;
+    private final OkHttpClient.Builder okBuilder;
+    private final AccessKey accesskey;
+    private final String servicePrincipalKey;
+    private final JSON json;
+
+    private AttributesApi attributesApi;
+    private AuditReasonsApi auditReasonsApi;
+    private EntriesApi entriesApi;
+    private FieldDefinitionsApi fieldDefinitionsApi;
+    private RepositoriesApi repositoriesApi;
+    private SearchesApi searchesApi;
+    private ServerSessionApi serverSessionApi;
+    private SimpleSearchesApi simpleSearchesApi;
+    private TagDefinitionsApi tagDefinitionsApi;
+    private TasksApi tasksApi;
+    private TemplateDefinitionsApi templateDefinitionsApi;
+
+    protected RepositoryApiClientImpl(String servicePrincipalKey, AccessKey accessKey, String baseUrlDebug) {
+        json = new JSON();
+
+        this.oauthHandler = new OAuthClientCredentialsHandler(servicePrincipalKey, accessKey);
+        this.accesskey = accessKey;
+        this.servicePrincipalKey = servicePrincipalKey;
+
+        String baseUrl = baseUrlDebug != null ? baseUrlDebug : accessKey.getDomain() + "repository/";
+
+        okBuilder = new OkHttpClient.Builder();
+        clientBuilder = new Retrofit
+                .Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonCustomConverterFactory.create(json.getGson()));
+    }
+
+    public static RepositoryApiClient CreateFromAccessKey(String servicePrincipalKey, AccessKey accessKey, String baseUrlDebug) {
+        return new RepositoryApiClientImpl(servicePrincipalKey, accessKey, baseUrlDebug);
+    }
+
+    public static RepositoryApiClient CreateFromAccessKey(String servicePrincipalKey, AccessKey accessKey) {
+        return CreateFromAccessKey(servicePrincipalKey, accessKey, null);
+    }
+
+    private <C> C createClient(Class<C> clientInterface) {
+        return clientBuilder.client(okBuilder.build())
+                .build()
+                .create(clientInterface);
+    }
+
+    @Override
+    public AttributesApi getAttributesClient() {
+        if (attributesApi != null) {
+            attributesApi = createClient(AttributesApi.class);
+        }
+        return attributesApi;
+    }
+
+    @Override
+    public AuditReasonsApi getAuditReasonsClient() {
+        if (auditReasonsApi != null) {
+            auditReasonsApi = createClient(AuditReasonsApi.class);
+        }
+        return auditReasonsApi;
+    }
+
+    @Override
+    public EntriesApi getEntriesClient() {
+        if (entriesApi != null) {
+            entriesApi = createClient(EntriesApi.class);
+        }
+        return entriesApi;
+    }
+
+    @Override
+    public FieldDefinitionsApi getFieldDefinitionsClient() {
+        if (fieldDefinitionsApi != null) {
+            fieldDefinitionsApi = createClient(FieldDefinitionsApi.class);
+        }
+        return fieldDefinitionsApi;
+    }
+
+    @Override
+    public RepositoriesApi getRepositoryClient() {
+        if (repositoriesApi != null) {
+            repositoriesApi = createClient(RepositoriesApi.class);
+        }
+        return repositoriesApi;
+    }
+
+    @Override
+    public SearchesApi getSearchesClient() {
+        if (searchesApi != null) {
+            searchesApi = createClient(SearchesApi.class);
+        }
+        return searchesApi;
+    }
+
+    @Override
+    public ServerSessionApi getServerSessionClient() {
+        if (serverSessionApi != null) {
+            serverSessionApi = createClient(ServerSessionApi.class);
+        }
+        return serverSessionApi;
+    }
+
+    @Override
+    public SimpleSearchesApi getSimpleSearchesClient() {
+        if (simpleSearchesApi != null) {
+            simpleSearchesApi = createClient(SimpleSearchesApi.class);
+        }
+        return simpleSearchesApi;
+    }
+
+    @Override
+    public TagDefinitionsApi getTagDefinitionsClient() {
+        if (tagDefinitionsApi != null) {
+            tagDefinitionsApi = createClient(TagDefinitionsApi.class);
+        }
+        return tagDefinitionsApi;
+    }
+
+    @Override
+    public TasksApi getTasksClient() {
+        if (tasksApi != null) {
+            tasksApi = createClient(TasksApi.class);
+        }
+        return tasksApi;
+    }
+
+    @Override
+    public TemplateDefinitionsApi getTemplateDefinitionClient() {
+        if (templateDefinitionsApi != null) {
+            templateDefinitionsApi = createClient(TemplateDefinitionsApi.class);
+        }
+        return templateDefinitionsApi;
+    }
+}
