bd2790d8047fce48698f6665b8181b8f867ee357
Jason Jia <jason.jia@laserfiche.com>
2022-06-22 15:55:08 -0400

    Add base test

---
+++ src/test/java/integration/BaseTest.java
@@ -0,0 +1,40 @@
+package integration;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.laserfiche.api.client.model.AccessKey;
+import com.nimbusds.jose.jwk.JWK;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.Base64;
+
+public class BaseTest {
+    protected static String spKey;
+    protected static AccessKey accessKey;
+
+    @BeforeAll
+    public static void setUp() {
+        // Load environment variables
+        Dotenv dotenv = Dotenv
+                .configure()
+                .filename(".env")
+                .load();
+
+        // Read env variables
+        Gson gson = new GsonBuilder().registerTypeAdapter(JWK.class, new JwkDeserializer()).create();
+
+        String accessKeyBase64 = dotenv.get("ACCESS_KEY");
+        String accessKeyStr = decodeBase64(accessKeyBase64);
+        // Gson doesn't escape forward slash https://github.com/google/gson/issues/356
+        accessKeyStr = accessKeyStr.replace("\\\"", "\"");
+
+        accessKey = gson.fromJson(accessKeyStr, AccessKey.class);
+        spKey = dotenv.get("SERVICE_PRINCIPAL_KEY");
+    }
+
+    private static String decodeBase64(String encoded) {
+        byte[] decodedBytes = Base64.getDecoder().decode(encoded);
+        return new String(decodedBytes);
+    }
+}
