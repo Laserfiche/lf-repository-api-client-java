a2e3f78e2123de99714cb75afe6fa065ba854bcd
Jason Jia <jason.jia@laserfiche.com>
2022-06-28 10:17:50 -0400

    Odata context classes are the only one we will ever return so remove them from the type selector
    - Also, because of this, the returned JSON will not container @Odata.type information, making the type selector crash

--- src/main/java/com/laserfiche/repository/api/ClientJson.java
+++ src/main/java/com/laserfiche/repository/api/ClientJson.java
@@ -19,15 +19,6 @@ public class ClientJson {
         GsonFireBuilder fireBuilder = new GsonFireBuilder()
                 .registerTypeSelector(Entry.class, new RepositoryApiReturnTypeSelector<>(Entry.class))
                 .registerTypeSelector(EntryFieldValue.class, new RepositoryApiReturnTypeSelector<>(EntryFieldValue.class))
-                .registerTypeSelector(ODataValueOfIListOfContextHit.class, new RepositoryApiReturnTypeSelector<>(ODataValueOfIListOfContextHit.class))
-                .registerTypeSelector(ODataValueOfIListOfEntry.class, new RepositoryApiReturnTypeSelector<>(ODataValueOfIListOfEntry.class))
-                .registerTypeSelector(ODataValueOfIListOfFieldValue.class, new RepositoryApiReturnTypeSelector<>(ODataValueOfIListOfFieldValue.class))
-                .registerTypeSelector(ODataValueOfIListOfTemplateFieldInfo.class, new RepositoryApiReturnTypeSelector<>(ODataValueOfIListOfTemplateFieldInfo.class))
-                .registerTypeSelector(ODataValueOfIListOfWEntryLinkInfo.class, new RepositoryApiReturnTypeSelector<>(ODataValueOfIListOfWEntryLinkInfo.class))
-                .registerTypeSelector(ODataValueOfIListOfWFieldInfo.class, new RepositoryApiReturnTypeSelector<>(ODataValueOfIListOfWFieldInfo.class))
-                .registerTypeSelector(ODataValueOfIListOfWTagInfo.class, new RepositoryApiReturnTypeSelector<>(ODataValueOfIListOfWTagInfo.class))
-                .registerTypeSelector(ODataValueOfIListOfWTemplateInfo.class, new RepositoryApiReturnTypeSelector<>(ODataValueOfIListOfWTemplateInfo.class))
-                .registerTypeSelector(ODataValueOfListOfAttribute.class, new RepositoryApiReturnTypeSelector<>(ODataValueOfListOfAttribute.class))
                 .registerTypeSelector(WFieldInfo.class, new RepositoryApiReturnTypeSelector<>(WFieldInfo.class));
         gson = fireBuilder.createGsonBuilder()
                 .registerTypeAdapter(Date.class, new JSON.DateTypeAdapter())
@@ -42,7 +33,7 @@ public class ClientJson {
     }
 
     static class RepositoryApiReturnTypeSelector<T> implements TypeSelector<T> {
-        private Map<String, Class> typeSelectableClasses;
+        private final Map<String, Class> typeSelectableClasses;
 
         public RepositoryApiReturnTypeSelector(Class<T> type) {
             typeSelectableClasses = new HashMap<>();
@@ -50,33 +41,6 @@ public class ClientJson {
             if (type == WFieldInfo.class) {
                 typeSelectableClasses.put("WFieldInfo", WFieldInfo.class);
                 typeSelectableClasses.put("TemplateFieldInfo", TemplateFieldInfo.class);
-            } else if (type == ODataValueOfListOfAttribute.class) {
-                typeSelectableClasses.put("ODataValueOfListOfAttribute", ODataValueOfListOfAttribute.class);
-                typeSelectableClasses.put("ODataValueContextOfListOfAttribute", ODataValueContextOfListOfAttribute.class);
-            } else if (type == ODataValueOfIListOfWTemplateInfo.class) {
-                typeSelectableClasses.put("ODataValueOfIListOfWTemplateInfo", ODataValueOfIListOfWTemplateInfo.class);
-                typeSelectableClasses.put("ODataValueContextOfIListOfWTemplateInfo", ODataValueContextOfIListOfWTemplateInfo.class);
-            } else if (type == ODataValueOfIListOfWTagInfo.class) {
-                typeSelectableClasses.put("ODataValueOfIListOfWTagInfo", ODataValueOfIListOfWTagInfo.class);
-                typeSelectableClasses.put("ODataValueContextOfIListOfWTagInfo", ODataValueContextOfIListOfWTagInfo.class);
-            } else if (type == ODataValueOfIListOfWFieldInfo.class) {
-                typeSelectableClasses.put("ODataValueOfIListOfWFieldInfo", ODataValueOfIListOfWFieldInfo.class);
-                typeSelectableClasses.put("ODataValueContextOfIListOfWFieldInfo", ODataValueContextOfIListOfWFieldInfo.class);
-            } else if (type == ODataValueOfIListOfWEntryLinkInfo.class) {
-                typeSelectableClasses.put("ODataValueOfIListOfWEntryLinkInfo", ODataValueOfIListOfWEntryLinkInfo.class);
-                typeSelectableClasses.put("ODataValueContextOfIListOfWEntryLinkInfo", ODataValueContextOfIListOfWEntryLinkInfo.class);
-            } else if (type == ODataValueOfIListOfTemplateFieldInfo.class) {
-                typeSelectableClasses.put("ODataValueOfIListOfTemplateFieldInfo", ODataValueOfIListOfTemplateFieldInfo.class);
-                typeSelectableClasses.put("ODataValueContextOfIListOfTemplateFieldInfo", ODataValueContextOfIListOfTemplateFieldInfo.class);
-            } else if (type == ODataValueOfIListOfFieldValue.class) {
-                typeSelectableClasses.put("ODataValueOfIListOfFieldValue", ODataValueOfIListOfFieldValue.class);
-                typeSelectableClasses.put("ODataValueContextOfIListOfFieldValue", ODataValueContextOfIListOfFieldValue.class);
-            } else if (type == ODataValueOfIListOfEntry.class) {
-                typeSelectableClasses.put("ODataValueOfIListOfEntry", ODataValueOfIListOfEntry.class);
-                typeSelectableClasses.put("ODataValueContextOfIListOfEntry", ODataValueContextOfIListOfEntry.class);
-            } else if (type == ODataValueOfIListOfContextHit.class) {
-                typeSelectableClasses.put("ODataValueOfIListOfContextHit", ODataValueOfIListOfContextHit.class);
-                typeSelectableClasses.put("ODataValueContextOfIListOfContextHit", ODataValueContextOfIListOfContextHit.class);
             } else if (type == EntryFieldValue.class) {
                 typeSelectableClasses.put("EntryFieldValue", EntryFieldValue.class);
                 typeSelectableClasses.put("FieldValue", FieldValue.class);
@@ -101,12 +65,12 @@ public class ClientJson {
             String typeComponentStr = element.getAsString();
             String[] typeComponents = typeComponentStr.split("\\.");
             String type = typeComponents[typeComponents.length - 1];
+            Class<T> classToDeserializeTo = typeSelectableClasses.get(type);
 
-            Class<T> classToDeserialize = typeSelectableClasses.get(type);
-            if (classToDeserialize == null) {
+            if (classToDeserializeTo == null) {
                 throw new RuntimeException(String.format("Cannot find class: %s", type));
             }
-            return classToDeserialize;
+            return classToDeserializeTo;
         }
     }
 }
