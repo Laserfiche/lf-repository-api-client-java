bbbe2206796a5413f1bcba8d943a499d679db28d
Jason Jia <jason.jia@laserfiche.com>
2022-06-28 11:48:42 -0400

    Add minimal test for simple search APIs

---
+++ src/test/java/integration/SimpleSearchesApiTest.java
@@ -0,0 +1,25 @@
+package integration;
+
+import com.laserfiche.repository.api.client.SimpleSearchesApi;
+import com.laserfiche.repository.api.client.model.ODataValueOfIListOfEntry;
+import com.laserfiche.repository.api.client.model.SimpleSearchRequest;
+import org.junit.jupiter.api.Test;
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SimpleSearchesApiTest extends BaseTest {
+    @Test
+    public void createSimpleSearchOperation_Success() throws IOException {
+        SimpleSearchesApi simpleSearchesClient = repositoryApiClient.getSimpleSearchesClient();
+        SimpleSearchRequest searchRequest = new SimpleSearchRequest();
+        searchRequest.setSearchCommand("({LF:Basic ~= \"search text\", option=\"DFANLT\"})");
+        Call<ODataValueOfIListOfEntry> call = simpleSearchesClient.createSimpleSearchOperation(repoId, searchRequest, null, null, null, null, null, false);
+        Response<ODataValueOfIListOfEntry> response = call.execute();
+
+        assertNotNull(response.body());
+    }
+}
