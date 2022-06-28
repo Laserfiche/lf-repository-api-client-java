62717284457f9277fa72f3c15068a0b1f77fe116
Jason Jia <jason.jia@laserfiche.com>
2022-06-28 10:30:25 -0400

    Add test to ensure type selector works as expected

--- src/test/java/integration/EntriesApiTest.java
+++ src/test/java/integration/EntriesApiTest.java
@@ -2,6 +2,7 @@ package integration;
 
 import com.laserfiche.repository.api.client.EntriesApi;
 import com.laserfiche.repository.api.client.model.Entry;
+import com.laserfiche.repository.api.client.model.ODataValueContextOfIListOfFieldValue;
 import org.junit.jupiter.api.Test;
 import retrofit2.Call;
 import retrofit2.Response;
@@ -17,6 +18,15 @@ public class EntriesApiTest extends BaseTest {
         EntriesApi entriesClient = repositoryApiClient.getEntriesClient();
         Call<Entry> call = entriesClient.getEntry(repoId, 1, null);
         Response<Entry> entry = call.execute();
+
+        assertNotNull(entry.body());
+    }
+
+    @Test
+    public void getFieldValues_Success() throws IOException {
+        EntriesApi entriesClient = repositoryApiClient.getEntriesClient();
+        Call<ODataValueContextOfIListOfFieldValue> call = entriesClient.getFieldValues(repoId, 1, null, null, null, null, null, null, null, false);
+        Response<ODataValueContextOfIListOfFieldValue> entry = call.execute();
 
         assertNotNull(entry.body());
     }
