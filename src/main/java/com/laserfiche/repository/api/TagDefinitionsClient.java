06660c876dd5d9ad3ebb895de93e71c4e41ccec6
Jason Jia <jason.jia@laserfiche.com>
2022-06-30 15:37:08 -0400

    Add a forEach implementation for tag definitions client

--- src/main/java/com/laserfiche/repository/api/TagDefinitionsClient.java
+++ src/main/java/com/laserfiche/repository/api/TagDefinitionsClient.java
@@ -2,6 +2,7 @@ package com.laserfiche.repository.api;
 
 import com.laserfiche.repository.api.client.TagDefinitionsApi;
 import com.laserfiche.repository.api.client.model.ODataValueContextOfIListOfWTagInfo;
+import com.laserfiche.repository.api.client.model.ODataValueContextOfListOfAttribute;
 import com.laserfiche.repository.api.client.model.WTagInfo;
 
 import java.util.concurrent.CompletableFuture;
@@ -46,5 +47,35 @@ public class TagDefinitionsClient extends BaseClient<TagDefinitionsApi> {
      */
     public CompletableFuture<ODataValueContextOfIListOfWTagInfo> getTagDefinitionsNextLink(String nextLink, Integer maxPageSize) {
         return client.getTagDefinitionsPaginate(nextLink, mergeMaxPageSizeIntoPrefer(maxPageSize, null));
+    }
+
+    /**
+     *
+     * - Returns all tag definitions in the repository. - Provide a repository ID and get a paged listing of tag definitions available in the repository. Useful when trying to display all tag definitions available, not only tags assigned to a specific entry. - Default page size: 100. Allowed OData query options: Select | Count | OrderBy | Skip | Top | SkipToken | Prefer.
+     * @param callback A lambda that will be called each time new data is retrieved. Returns false to stop receiving more data; returns true to be called again if there's more data.
+     * @param repoId The requested repository ID. (required)
+     * @param prefer An optional OData header. Can be used to set the maximum page size using odata.maxpagesize. (optional)
+     * @param culture An optional query parameter used to indicate the locale that should be used for formatting.             The value should be a standard language tag. (optional)
+     * @param $select Limits the properties returned in the result. (optional)
+     * @param $orderby Specifies the order in which items are returned. The maximum number of expressions is 5. (optional)
+     * @param $top Limits the number of items returned from a collection. (optional)
+     * @param $skip Excludes the specified number of items of the queried collection from the result. (optional)
+     * @param $count Indicates whether the total count of items within a collection are returned in the result. (optional)
+     * @param maxPageSize Indicates the maximum number of items to return.
+     */
+    public void getTrusteeAttributeKeyValuePairsForEach(ForEachCallBack<CompletableFuture<ODataValueContextOfIListOfWTagInfo>> callback, String repoId, String prefer, String culture, String $select, String $orderby, Integer $top, Integer $skip, Boolean $count, Integer maxPageSize) {
+        // Initial request
+        CompletableFuture<ODataValueContextOfIListOfWTagInfo> future = getTagDefinitions(repoId, prefer, culture, $select, $orderby, $top, $skip, $count, maxPageSize);
+        // Subsequent request based on return value of callback
+        while (callback.apply(future)) {
+            future = future.thenCompose(dataFromLastRequest -> {
+                String nextLink = dataFromLastRequest.getAtOdataNextLink();
+                if (nextLink == null) {
+                    // We are at the end of the data stream
+                    return CompletableFuture.completedFuture(null);
+                }
+                return getTagDefinitionsNextLink(nextLink, maxPageSize);
+            });
+        }
     }
 }
