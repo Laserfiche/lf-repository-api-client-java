121ae2b631a6c56704a265265ea576477e637fcf
Jason Jia <jason.jia@laserfiche.com>
2022-07-27 09:52:10 -0400

    Update JavaDoc

--- src/main/java/com/laserfiche/repository/api/clients/TasksClient.java
+++ src/main/java/com/laserfiche/repository/api/clients/TasksClient.java
@@ -10,7 +10,7 @@ public interface TasksClient {
      * - Cancels an operation. - Provide an operationToken to cancel the operation, if possible. Should be used if an operation was created in error, or is no longer necessary. - Rollbacks must be done manually. For example, if a copy operation is started and is halfway complete when canceled, the client application is responsible for cleaning up the files that were successfully copied before the operation was canceled.
      * @param repoId The requested repository ID (required)
      * @param operationToken The operation token (required)
-     * @return CompletableFuture&lt;Void&gt;
+     * @return CompletableFuture&lt;Boolean&gt;
      */
     CompletableFuture<Boolean> cancelOperation(String repoId, String operationToken);
 
