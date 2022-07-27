29b7a5203a6a5fd6fb010982df5cb27c0828b31a
Jason Jia <jason.jia@laserfiche.com>
2022-07-27 10:14:39 -0400

    Fix JavaDoc

--- src/main/java/com/laserfiche/repository/api/clients/impl/TasksApiEx.java
+++ src/main/java/com/laserfiche/repository/api/clients/impl/TasksApiEx.java
@@ -11,10 +11,10 @@ public interface TasksApiEx {
      * - Cancels an operation. - Provide an operationToken to cancel the operation, if possible. Should be used if an operation was created in error, or is no longer necessary. - Rollbacks must be done manually. For example, if a copy operation is started and is halfway complete when canceled, the client application is responsible for cleaning up the files that were successfully copied before the operation was canceled.
      * @param repoId The requested repository ID (required)
      * @param operationToken The operation token (required)
-     * @return ResponseBody
+     * @return CompletableFuture&lt;ResponseBody&gt;
      */
     @DELETE("v1/Repositories/{repoId}/Tasks/{operationToken}")
-    CompletableFuture<Response> cancelOperation(
+    CompletableFuture<Response<Void>> cancelOperation(
             @retrofit2.http.Path("repoId") String repoId, @retrofit2.http.Path("operationToken") String operationToken
     );
 }
