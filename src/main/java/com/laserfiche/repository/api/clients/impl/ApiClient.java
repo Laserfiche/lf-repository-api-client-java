bd5ddbdb2cc6140d2ca7436929f0dc8df678ad1b
Jason Jia <jason.jia@laserfiche.com>
2022-08-30 10:07:43 -0400

    Set up the HTTP client in the parent class

--- src/main/java/com/laserfiche/repository/api/clients/impl/ApiClient.java
+++ src/main/java/com/laserfiche/repository/api/clients/impl/ApiClient.java
@@ -1,9 +1,13 @@
 package com.laserfiche.repository.api.clients.impl;
 
+import com.laserfiche.api.client.oauth.TokenClientObjectMapper;
+import kong.unirest.Unirest;
+
 public class ApiClient {
     public String baseUrl;
 
     public ApiClient(String baseUrl) {
         this.baseUrl = baseUrl;
+        Unirest.config().setObjectMapper(new TokenClientObjectMapper());
     }
 }
