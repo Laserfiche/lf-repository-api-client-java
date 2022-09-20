186b4bb6efed082e752e9e4f1c2cf55cac3dc11d
Samad Paydar <samad.paydar@laserfiche.com>
2022-09-20 12:36:58 -0400

    Add an integration test for nextLink support for search api.

--- src/test/java/integration/SearchApiTest.java
+++ src/test/java/integration/SearchApiTest.java
@@ -106,4 +106,39 @@ public class SearchApiTest extends BaseTest {
 
         assertTrue(closeSearchResponse.value);
     }
+
+     @Test
+    void getSearchResults_SimplePaging() throws InterruptedException {
+        int maxPageSize = 1;
+
+        AdvancedSearchRequest request = new AdvancedSearchRequest();
+        request.searchCommand = "({LF:Basic ~= \"*\", option=\"DFANLT\"})";
+
+        AcceptedOperation searchOperationResponse = client.createSearchOperation(repoId, request).join();
+
+        String searchToken = searchOperationResponse.token;
+        assertTrue(searchToken != null && !searchToken.trim().isEmpty());
+
+        TimeUnit.SECONDS.sleep(5);
+
+        CompletableFuture<ODataValueContextOfIListOfEntry> searchResultResponse = client.getSearchResults(repoId, searchToken, null, null, null, null, String.format("maxpagesize=%d", maxPageSize), null, null, null, null, null, null);
+        ODataValueContextOfIListOfEntry searchResults = searchResultResponse.join();
+
+        assertNotNull(searchResults);
+        if(searchResults.value.isEmpty()) {
+            return; // There's no point testing if we don't have any such item.
+        }
+
+        String nextLink = searchResults._atOdataNextLink;
+        assertNotNull(nextLink);
+        assertTrue(searchResults.value.size() <= maxPageSize);
+
+        // Paging request
+        searchResultResponse = client.getSearchResultsNextLink(nextLink, maxPageSize);
+        assertNotNull(searchResultResponse);
+        TimeUnit.SECONDS.sleep(5);
+        searchResults = searchResultResponse.join();
+        assertNotNull(searchResults);
+        assertTrue(searchResults.value.size() <= maxPageSize);
+    }
 }
