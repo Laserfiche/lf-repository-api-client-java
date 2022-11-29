78cb857d2fc4d60cc7111c8016d925b59ff262eb
Jason Jia <jason.jia@laserfiche.com>
2022-11-29 10:27:13 -0500

    Use blocking API for attributes client and update its tests

--- src/main/java/com/laserfiche/repository/api/clients/impl/AttributesClientImpl.java
+++ src/main/java/com/laserfiche/repository/api/clients/impl/AttributesClientImpl.java
@@ -5,13 +5,13 @@ import com.laserfiche.repository.api.clients.AttributesClient;
 import com.laserfiche.repository.api.clients.impl.model.Attribute;
 import com.laserfiche.repository.api.clients.impl.model.ODataValueContextOfListOfAttribute;
 import com.laserfiche.repository.api.clients.impl.model.ProblemDetails;
+import kong.unirest.HttpResponse;
 import kong.unirest.UnirestInstance;
 import kong.unirest.UnirestParsingException;
 import kong.unirest.json.JSONObject;
 
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -22,73 +22,66 @@ public class AttributesClientImpl extends ApiClient implements AttributesClient 
     }
 
     @Override
-    public CompletableFuture<Attribute> getTrusteeAttributeValueByKey(String repoId, String attributeKey,
-            Boolean everyone) {
+    public Attribute getTrusteeAttributeValueByKey(String repoId, String attributeKey, Boolean everyone) {
         Map<String, Object> queryParameters = getNonNullParameters(new String[]{"everyone"}, new Object[]{everyone});
         Map<String, Object> pathParameters = getNonNullParameters(new String[]{"repoId", "attributeKey"},
                 new Object[]{repoId, attributeKey});
-        return httpClient
+        HttpResponse<Object> httpResponse = httpClient
                 .get(baseUrl + "/v1/Repositories/{repoId}/Attributes/{attributeKey}")
                 .queryString(queryParameters)
                 .routeParam(pathParameters)
-                .asObjectAsync(Object.class)
-                .thenApply(httpResponse -> {
-                    Object body = httpResponse.getBody();
-                    if (httpResponse.getStatus() == 200) {
-                        try {
-                            String jsonString = new JSONObject(body).toString();
-                            return objectMapper.readValue(jsonString, Attribute.class);
-                        } catch (JsonProcessingException | IllegalStateException e) {
-                            e.printStackTrace();
-                            return null;
-                        }
-                    } else {
-                        ProblemDetails problemDetails;
-                        Map<String, String> headersMap = getHeadersMap(httpResponse.getHeaders());
-                        try {
-                            String jsonString = new JSONObject(body).toString();
-                            problemDetails = deserializeToProblemDetails(jsonString);
-                        } catch (JsonProcessingException | IllegalStateException e) {
-                            Optional<UnirestParsingException> parsingException = httpResponse.getParsingError();
-                            throw new ApiException(httpResponse.getStatusText(), httpResponse.getStatus(),
-                                    (parsingException.isPresent() ? parsingException
-                                            .get()
-                                            .getOriginalBody() : null), headersMap, null);
-                        }
-                        if (httpResponse.getStatus() == 400)
-                            throw new ApiException(decideErrorMessage(problemDetails, "Invalid or bad request."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else if (httpResponse.getStatus() == 401)
-                            throw new ApiException(
-                                    decideErrorMessage(problemDetails, "Access token is invalid or expired."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else if (httpResponse.getStatus() == 403)
-                            throw new ApiException(
-                                    decideErrorMessage(problemDetails, "Access denied for the operation."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else if (httpResponse.getStatus() == 404)
-                            throw new ApiException(
-                                    decideErrorMessage(problemDetails, "Requested attribute key not found."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else if (httpResponse.getStatus() == 429)
-                            throw new ApiException(decideErrorMessage(problemDetails, "Rate limit is reached."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else
-                            throw new RuntimeException(httpResponse.getStatusText());
-                    }
-                });
+                .asObject(Object.class);
+        Object body = httpResponse.getBody();
+        if (httpResponse.getStatus() == 200) {
+            try {
+                String jsonString = new JSONObject(body).toString();
+                return objectMapper.readValue(jsonString, Attribute.class);
+            } catch (JsonProcessingException | IllegalStateException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
+            ProblemDetails problemDetails;
+            Map<String, String> headersMap = getHeadersMap(httpResponse.getHeaders());
+            try {
+                String jsonString = new JSONObject(body).toString();
+                problemDetails = deserializeToProblemDetails(jsonString);
+            } catch (JsonProcessingException | IllegalStateException e) {
+                Optional<UnirestParsingException> parsingException = httpResponse.getParsingError();
+                throw new ApiException(httpResponse.getStatusText(), httpResponse.getStatus(),
+                        (parsingException.isPresent() ? parsingException
+                                .get()
+                                .getOriginalBody() : null), headersMap, null);
+            }
+            if (httpResponse.getStatus() == 400)
+                throw new ApiException(decideErrorMessage(problemDetails, "Invalid or bad request."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else if (httpResponse.getStatus() == 401)
+                throw new ApiException(decideErrorMessage(problemDetails, "Access token is invalid or expired."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else if (httpResponse.getStatus() == 403)
+                throw new ApiException(decideErrorMessage(problemDetails, "Access denied for the operation."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else if (httpResponse.getStatus() == 404)
+                throw new ApiException(decideErrorMessage(problemDetails, "Requested attribute key not found."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else if (httpResponse.getStatus() == 429)
+                throw new ApiException(decideErrorMessage(problemDetails, "Rate limit is reached."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else
+                throw new RuntimeException(httpResponse.getStatusText());
+        }
     }
 
     @Override
-    public CompletableFuture<ODataValueContextOfListOfAttribute> getTrusteeAttributeKeyValuePairs(String repoId,
-            Boolean everyone, String prefer, String select, String orderby, Integer top, Integer skip, Boolean count) {
+    public ODataValueContextOfListOfAttribute getTrusteeAttributeKeyValuePairs(String repoId, Boolean everyone,
+            String prefer, String select, String orderby, Integer top, Integer skip, Boolean count) {
         return doGetTrusteeAttributeKeyValuePairs(baseUrl + "/v1/Repositories/{repoId}/Attributes", repoId, everyone,
                 prefer, select, orderby, top, skip, count);
     }
 
-    private CompletableFuture<ODataValueContextOfListOfAttribute> doGetTrusteeAttributeKeyValuePairs(String url,
-            String repoId, Boolean everyone, String prefer, String select, String orderby, Integer top, Integer skip,
-            Boolean count) {
+    private ODataValueContextOfListOfAttribute doGetTrusteeAttributeKeyValuePairs(String url, String repoId,
+            Boolean everyone, String prefer, String select, String orderby, Integer top, Integer skip, Boolean count) {
         Map<String, Object> queryParameters = getNonNullParameters(
                 new String[]{"everyone", "$select", "$orderby", "$top", "$skip", "$count"},
                 new Object[]{everyone, select, orderby, top, skip, count});
@@ -98,81 +91,71 @@ public class AttributesClientImpl extends ApiClient implements AttributesClient 
                 .entrySet()
                 .stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, e -> (String) e.getValue()));
-        return httpClient
+        HttpResponse<Object> httpResponse = httpClient
                 .get(url)
                 .queryString(queryParameters)
                 .routeParam(pathParameters)
                 .headers(headerParametersWithStringTypeValue)
-                .asObjectAsync(Object.class)
-                .thenApply(httpResponse -> {
-                    Object body = httpResponse.getBody();
-                    if (httpResponse.getStatus() == 200) {
-                        try {
-                            String jsonString = new JSONObject(body).toString();
-                            return objectMapper.readValue(jsonString, ODataValueContextOfListOfAttribute.class);
-                        } catch (JsonProcessingException | IllegalStateException e) {
-                            e.printStackTrace();
-                            return null;
-                        }
-                    } else {
-                        ProblemDetails problemDetails;
-                        Map<String, String> headersMap = getHeadersMap(httpResponse.getHeaders());
-                        try {
-                            String jsonString = new JSONObject(body).toString();
-                            problemDetails = deserializeToProblemDetails(jsonString);
-                        } catch (JsonProcessingException | IllegalStateException e) {
-                            Optional<UnirestParsingException> parsingException = httpResponse.getParsingError();
-                            throw new ApiException(httpResponse.getStatusText(), httpResponse.getStatus(),
-                                    (parsingException.isPresent() ? parsingException
-                                            .get()
-                                            .getOriginalBody() : null), headersMap, null);
-                        }
-                        if (httpResponse.getStatus() == 400)
-                            throw new ApiException(decideErrorMessage(problemDetails, "Invalid or bad request."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else if (httpResponse.getStatus() == 401)
-                            throw new ApiException(
-                                    decideErrorMessage(problemDetails, "Access token is invalid or expired."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else if (httpResponse.getStatus() == 403)
-                            throw new ApiException(
-                                    decideErrorMessage(problemDetails, "Access denied for the operation."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else if (httpResponse.getStatus() == 404)
-                            throw new ApiException(decideErrorMessage(problemDetails, "Not found."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else if (httpResponse.getStatus() == 429)
-                            throw new ApiException(decideErrorMessage(problemDetails, "Rate limit is reached."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else
-                            throw new RuntimeException(httpResponse.getStatusText());
-                    }
-                });
+                .asObject(Object.class);
+        Object body = httpResponse.getBody();
+        if (httpResponse.getStatus() == 200) {
+            try {
+                String jsonString = new JSONObject(body).toString();
+                return objectMapper.readValue(jsonString, ODataValueContextOfListOfAttribute.class);
+            } catch (JsonProcessingException | IllegalStateException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
+            ProblemDetails problemDetails;
+            Map<String, String> headersMap = getHeadersMap(httpResponse.getHeaders());
+            try {
+                String jsonString = new JSONObject(body).toString();
+                problemDetails = deserializeToProblemDetails(jsonString);
+            } catch (JsonProcessingException | IllegalStateException e) {
+                Optional<UnirestParsingException> parsingException = httpResponse.getParsingError();
+                throw new ApiException(httpResponse.getStatusText(), httpResponse.getStatus(),
+                        (parsingException.isPresent() ? parsingException
+                                .get()
+                                .getOriginalBody() : null), headersMap, null);
+            }
+            if (httpResponse.getStatus() == 400)
+                throw new ApiException(decideErrorMessage(problemDetails, "Invalid or bad request."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else if (httpResponse.getStatus() == 401)
+                throw new ApiException(decideErrorMessage(problemDetails, "Access token is invalid or expired."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else if (httpResponse.getStatus() == 403)
+                throw new ApiException(decideErrorMessage(problemDetails, "Access denied for the operation."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else if (httpResponse.getStatus() == 404)
+                throw new ApiException(decideErrorMessage(problemDetails, "Not found."), httpResponse.getStatus(),
+                        httpResponse.getStatusText(), headersMap, problemDetails);
+            else if (httpResponse.getStatus() == 429)
+                throw new ApiException(decideErrorMessage(problemDetails, "Rate limit is reached."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else
+                throw new RuntimeException(httpResponse.getStatusText());
+        }
     }
 
     @Override
-    public CompletableFuture<ODataValueContextOfListOfAttribute> getTrusteeAttributeKeyValuePairsNextLink(
-            String nextLink, Integer maxPageSize) {
+    public ODataValueContextOfListOfAttribute getTrusteeAttributeKeyValuePairsNextLink(String nextLink,
+            Integer maxPageSize) {
         return doGetTrusteeAttributeKeyValuePairs(nextLink, null, null, mergeMaxSizeIntoPrefer(maxPageSize, null), null,
                 null, null, null, null);
     }
 
     @Override
-    public CompletableFuture<Void> getTrusteeAttributeKeyValuePairsForEach(
-            Function<CompletableFuture<ODataValueContextOfListOfAttribute>, CompletableFuture<Boolean>> callback,
+    public void getTrusteeAttributeKeyValuePairsForEach(Function<ODataValueContextOfListOfAttribute, Boolean> callback,
             Integer maxPageSize, String repoId, Boolean everyone, String prefer, String select, String orderby,
             Integer top, Integer skip, Boolean count) {
         prefer = mergeMaxSizeIntoPrefer(maxPageSize, prefer);
-        CompletableFuture<ODataValueContextOfListOfAttribute> response = getTrusteeAttributeKeyValuePairs(repoId,
-                everyone, prefer, select, orderby, top, skip, count);
-        while (response != null && callback
-                .apply(response)
-                .join()) {
-            String nextLink = response
-                    .join()
-                    .getOdataNextLink();
+        ODataValueContextOfListOfAttribute response = getTrusteeAttributeKeyValuePairs(repoId, everyone, prefer, select,
+                orderby, top, skip, count);
+        while (response != null && callback.apply(response)) {
+            String nextLink = response.getOdataNextLink();
             response = getTrusteeAttributeKeyValuePairsNextLink(nextLink, maxPageSize);
         }
-        return CompletableFuture.completedFuture(null);
     }
 }
