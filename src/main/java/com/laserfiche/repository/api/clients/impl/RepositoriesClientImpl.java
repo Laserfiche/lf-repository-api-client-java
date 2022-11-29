5b6771a2614f3b82a74b0a4f2ee570424d22701b
Jason Jia <jason.jia@laserfiche.com>
2022-11-29 14:47:23 -0500

    Use blocking API for repositories client, its implementation, and tests

--- src/main/java/com/laserfiche/repository/api/clients/impl/RepositoriesClientImpl.java
+++ src/main/java/com/laserfiche/repository/api/clients/impl/RepositoriesClientImpl.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.laserfiche.repository.api.clients.RepositoriesClient;
 import com.laserfiche.repository.api.clients.impl.model.ProblemDetails;
 import com.laserfiche.repository.api.clients.impl.model.RepositoryInfo;
+import kong.unirest.HttpResponse;
 import kong.unirest.UnirestInstance;
 import kong.unirest.UnirestParsingException;
 import kong.unirest.json.JSONArray;
@@ -12,7 +13,6 @@ import kong.unirest.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
 
 public class RepositoriesClientImpl extends ApiClient implements RepositoriesClient {
 
@@ -21,50 +21,46 @@ public class RepositoriesClientImpl extends ApiClient implements RepositoriesCli
     }
 
     @Override
-    public CompletableFuture<RepositoryInfo[]> getRepositoryList() {
-        return httpClient
+    public RepositoryInfo[] getRepositoryList() {
+        HttpResponse<Object> httpResponse = httpClient
                 .get(baseUrl + "/v1/Repositories")
-                .asObjectAsync(Object.class)
-                .thenApply(httpResponse -> {
-                    Object body = httpResponse.getBody();
-                    if (httpResponse.getStatus() == 200) {
-                        try {
-                            String jsonString = new JSONArray(((ArrayList) body).toArray()).toString();
-                            return objectMapper.readValue(jsonString, RepositoryInfo[].class);
-                        } catch (JsonProcessingException | IllegalStateException e) {
-                            e.printStackTrace();
-                            return null;
-                        }
-                    } else {
-                        ProblemDetails problemDetails;
-                        Map<String, String> headersMap = getHeadersMap(httpResponse.getHeaders());
-                        try {
-                            String jsonString = new JSONObject(body).toString();
-                            problemDetails = deserializeToProblemDetails(jsonString);
-                        } catch (JsonProcessingException | IllegalStateException e) {
-                            Optional<UnirestParsingException> parsingException = httpResponse.getParsingError();
-                            throw new ApiException(httpResponse.getStatusText(), httpResponse.getStatus(),
-                                    (parsingException.isPresent() ? parsingException
-                                            .get()
-                                            .getOriginalBody() : null), headersMap, null);
-                        }
-                        if (httpResponse.getStatus() == 400)
-                            throw new ApiException(decideErrorMessage(problemDetails, "Invalid or bad request."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else if (httpResponse.getStatus() == 401)
-                            throw new ApiException(
-                                    decideErrorMessage(problemDetails, "Access token is invalid or expired."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else if (httpResponse.getStatus() == 403)
-                            throw new ApiException(
-                                    decideErrorMessage(problemDetails, "Access denied for the operation."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else if (httpResponse.getStatus() == 429)
-                            throw new ApiException(decideErrorMessage(problemDetails, "Rate limit is reached."),
-                                    httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
-                        else
-                            throw new RuntimeException(httpResponse.getStatusText());
-                    }
-                });
+                .asObject(Object.class);
+        Object body = httpResponse.getBody();
+        if (httpResponse.getStatus() == 200) {
+            try {
+                String jsonString = new JSONArray(((ArrayList) body).toArray()).toString();
+                return objectMapper.readValue(jsonString, RepositoryInfo[].class);
+            } catch (JsonProcessingException | IllegalStateException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
+            ProblemDetails problemDetails;
+            Map<String, String> headersMap = getHeadersMap(httpResponse.getHeaders());
+            try {
+                String jsonString = new JSONObject(body).toString();
+                problemDetails = deserializeToProblemDetails(jsonString);
+            } catch (JsonProcessingException | IllegalStateException e) {
+                Optional<UnirestParsingException> parsingException = httpResponse.getParsingError();
+                throw new ApiException(httpResponse.getStatusText(), httpResponse.getStatus(),
+                        (parsingException.isPresent() ? parsingException
+                                .get()
+                                .getOriginalBody() : null), headersMap, null);
+            }
+            if (httpResponse.getStatus() == 400)
+                throw new ApiException(decideErrorMessage(problemDetails, "Invalid or bad request."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else if (httpResponse.getStatus() == 401)
+                throw new ApiException(decideErrorMessage(problemDetails, "Access token is invalid or expired."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else if (httpResponse.getStatus() == 403)
+                throw new ApiException(decideErrorMessage(problemDetails, "Access denied for the operation."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else if (httpResponse.getStatus() == 429)
+                throw new ApiException(decideErrorMessage(problemDetails, "Rate limit is reached."),
+                        httpResponse.getStatus(), httpResponse.getStatusText(), headersMap, problemDetails);
+            else
+                throw new RuntimeException(httpResponse.getStatusText());
+        }
     }
 }
