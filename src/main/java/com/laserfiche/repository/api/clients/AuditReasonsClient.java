2a9460a14d6d60a8f6247419238fe90314d5ac49
Jason Jia <jason.jia@laserfiche.com>
2022-11-29 10:30:24 -0500

    Update audit reasons client, its implementation, and tests to use blocking API

--- src/main/java/com/laserfiche/repository/api/clients/AuditReasonsClient.java
+++ src/main/java/com/laserfiche/repository/api/clients/AuditReasonsClient.java
@@ -2,8 +2,6 @@ package com.laserfiche.repository.api.clients;
 
 import com.laserfiche.repository.api.clients.impl.model.AuditReasons;
 
-import java.util.concurrent.CompletableFuture;
-
 public interface AuditReasonsClient {
 
     /**
@@ -12,7 +10,7 @@ public interface AuditReasonsClient {
      * - If the authenticated user does not have the appropriate Laserfiche feature right, the audit reasons associated with that feature right will not be included.
      *
      * @param repoId The requested repository ID.
-     * @return CompletableFuture&lt;AuditReasons&gt; The return value
+     * @return AuditReasons The return value
      */
-    CompletableFuture<AuditReasons> getAuditReasons(String repoId);
+    AuditReasons getAuditReasons(String repoId);
 }
