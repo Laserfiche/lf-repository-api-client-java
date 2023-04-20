2b5e37a314e0339a5b3c06924bf7764c19edfe42
Steven Xu <103658932+steven-xu-lf@users.noreply.github.com>
2023-04-20 14:47:16 -0400

    added initial unit tests for apiclientUtil helper functions (#102)
    * added initial unit tests for apiclientUtil helper functions
    
    * fixed and refactored new unit tests
    
    * refactored and added additional unit tests
    
    * refactored existing unit tests
    
    * added additional unit tests
    
    * renamed unit test
    
    * created a new ApiClientUtils folder and moved everything to that folder

---
+++ src/test/java/unit/clients/impl/ApiClientUtils/GetHeaders.java
@@ -0,0 +1,109 @@
+package unit.clients.impl.ApiClientUtils;
+
+import com.laserfiche.repository.api.clients.impl.ApiClientUtils;
+import kong.unirest.Headers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class GetHeaders {
+    private final Map<String, String> headerParametersWithStringTypeValue = new HashMap<String, String>();
+    private Headers header;
+
+    @BeforeEach
+    void headerSetup() {
+        header = new Headers();
+    }
+
+    @AfterEach
+    void clearHeaders() {
+        headerParametersWithStringTypeValue.clear();
+        header.clear();
+    }
+
+    @Test
+    void getHeadersMap_AllNullParameters() {
+        Assertions.assertThrows(
+                NullPointerException.class, () -> ApiClientUtils.getHeadersMap(null));
+    }
+
+    @Test
+    void getHeadersMap_SingleKeyValueHeaderMap() {
+        headerParametersWithStringTypeValue.put("test", "test");
+        header.add(headerParametersWithStringTypeValue);
+        Map<String, String> result = ApiClientUtils.getHeadersMap(header);
+        assertNotNull(headerParametersWithStringTypeValue);
+        assertEquals(result, headerParametersWithStringTypeValue);
+    }
+
+    @Test
+    void getHeadersMap_MultipleKeyValueHeaderMap() {
+        for (int i = 0; i < 5; i++) {
+            headerParametersWithStringTypeValue.put(String.format("test%s", i), "test");
+        }
+        header.add(headerParametersWithStringTypeValue);
+        Map<String, String> result = ApiClientUtils.getHeadersMap(header);
+        assertNotNull(headerParametersWithStringTypeValue);
+        assertEquals(result, headerParametersWithStringTypeValue);
+    }
+
+    @Test
+    void getHeadersMap_NullHeaderMap() {
+        headerParametersWithStringTypeValue.put(null, null);
+        header.add(headerParametersWithStringTypeValue);
+        Map<String, String> result = ApiClientUtils.getHeadersMap(header);
+        assertNotNull(headerParametersWithStringTypeValue);
+        assertEquals(result, new HashMap<String, String>());
+    }
+
+    @Test
+    void getHeadersMap_EmptyStringHeaderMap() {
+        headerParametersWithStringTypeValue.put("", "");
+        header.add(headerParametersWithStringTypeValue);
+        Map<String, String> result = ApiClientUtils.getHeadersMap(header);
+        assertNotNull(headerParametersWithStringTypeValue);
+        assertEquals(result, headerParametersWithStringTypeValue);
+    }
+
+    @Test
+    void getHeaderMap_AddKeyToHeaderMap(){
+        for (int i = 0; i < 5; i++) {
+            headerParametersWithStringTypeValue.put(String.format("test%s", i), "test");
+        }
+        header.add(headerParametersWithStringTypeValue);
+        Map<String, String> result = ApiClientUtils.getHeadersMap(header);
+        assertNotNull(headerParametersWithStringTypeValue);
+        assertEquals(result, headerParametersWithStringTypeValue);
+        header.clear();
+        headerParametersWithStringTypeValue.put("test6", "test");
+        header.add(headerParametersWithStringTypeValue);
+        result = ApiClientUtils.getHeadersMap(header);
+        assertNotNull(headerParametersWithStringTypeValue);
+        assertEquals(result, headerParametersWithStringTypeValue);
+    }
+
+    @Test
+    void getHeaderMap_DeleteKeyToHeaderMap(){
+        for (int i = 0; i < 5; i++) {
+            headerParametersWithStringTypeValue.put(String.format("test%s", i), "test");
+        }
+        header.add(headerParametersWithStringTypeValue);
+        Map<String, String> result = ApiClientUtils.getHeadersMap(header);
+        assertNotNull(headerParametersWithStringTypeValue);
+        assertEquals(result, headerParametersWithStringTypeValue);
+        header.clear();
+        headerParametersWithStringTypeValue.remove(1);
+        header.add(headerParametersWithStringTypeValue);
+        result = ApiClientUtils.getHeadersMap(header);
+        assertNotNull(headerParametersWithStringTypeValue);
+        assertEquals(result, headerParametersWithStringTypeValue);
+    }
+
+}
