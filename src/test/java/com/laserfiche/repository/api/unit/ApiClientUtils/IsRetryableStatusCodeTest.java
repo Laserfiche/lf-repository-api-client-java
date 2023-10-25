b5f40b9d2bc9bc99b0df8c6a881f0c6e893788af
Samad Paydar <samad.paydar@laserfiche.com>
2023-10-25 09:56:11 -0400

    Add copyright header.

--- src/test/java/com/laserfiche/repository/api/unit/ApiClientUtils/IsRetryableStatusCodeTest.java
+++ src/test/java/com/laserfiche/repository/api/unit/ApiClientUtils/IsRetryableStatusCodeTest.java
@@ -1,3 +1,5 @@
+// Copyright (c) Laserfiche.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 package com.laserfiche.repository.api.unit.ApiClientUtils;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
